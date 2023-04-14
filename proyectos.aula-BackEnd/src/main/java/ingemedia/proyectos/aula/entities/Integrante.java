f95395bec62e8ee351d959c9f5d4cbbf5cda9ed8
elkingrana2 <elkingc1@gmail.com>
2023-04-14 15:58:28 -0500

    cambios

--- proyectos.aula-BackEnd/src/main/java/ingemedia/proyectos/aula/entities/Integrante.java
+++ proyectos.aula-BackEnd/src/main/java/ingemedia/proyectos/aula/entities/Integrante.java
@@ -2,7 +2,6 @@ package ingemedia.proyectos.aula.entities;
 
 import ingemedia.proyectos.aula.request.IntegranteRequest;
 import ingemedia.proyectos.aula.request.Rol;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
