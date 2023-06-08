8e196799fe8d76223f9351375015b85b6d40a4e2
NATALIA ORTIZ ARMESTO <nataliaoa@ufps.edu.co>
2023-06-08 06:01:04 -0500

    Add Sidebar and Image Profile

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { AuthService } from '@core/auth/auth.service';
+import { ISidebar } from '@data/interfaces/ui/sidebar.interface';
+import { ROLE } from '@data/enums/role.enum';
+import { User } from '@data/models/User.model';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,22 +12,23 @@ import { AuthService } from '@core/auth/auth.service';
 })
 export class SidebarComponent implements OnInit {
 
-  @Input() menuItems!: any[];
-  public currentUser: any;
+  @Input() menuItems: ISidebar[];
+  public currentUser: User;
 
   constructor(private authService: AuthService) {
+    this.menuItems = [];
+    this.currentUser = this.authService.getUser();
   }
 
   ngOnInit(): void { }
 
-  public hasRole(roles: any[]): boolean {
+  public hasRole(roles: ROLE[]): boolean {
     if (!roles) return true;
-    return true;
-    //return this.authService.hasRole(roles);
+    return this.authService.hasRole(roles);
   }
 
   public logOut(): void {
-    //this.authService.logout();
+    this.authService.logOut();
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+const SERVER = {
+  protocol: 'http',
+  hostname: '3.15.147.189',
+  port: 8090,
+};
+
 export const environment = {
-  production: true
+  production: true,
+  encrypt: true,
+  keyEcrypt: 'Fr0nT3nD2023',
+  baseUrlAuth: `${SERVER.protocol}://${SERVER.hostname}:${SERVER.port}/api/v1/auth`,
+  baseUrlProjects: `${SERVER.protocol}://${SERVER.hostname}:${SERVER.port}/api/v1/proyectos`,
+  baseUrlMembers: `${SERVER.protocol}://${SERVER.hostname}:${SERVER.port}/api/v1/integrantes`,
 };
